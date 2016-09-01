1192c364ea6f32565f9258ab566c1cabf9a174bd
RajuKoushik <g.rajukoushik@gmail.com>
2016-09-01 14:26:27 +0530

    Updated blog with individual posts on a new view.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<post_id>[0-9]+)/$', views.indi_post , name='indi_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.http import Http404,HttpResponse,HttpResponseRedirect
+from django.template import RequestContext, loader
+
+from django.shortcuts import get_object_or_404 , render
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def indi_post(request , post_id):
+
+    try:
+        post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+
+    return render(request, 'blog/indi_post.html', {'post' : post})
