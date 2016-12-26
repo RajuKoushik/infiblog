207bc1885cda5b47c1397fc3e37f82c4a43346a8
rajukoushik <g.rajukoushik@gmail.com>
2016-12-26 22:59:55 +0530

    View update

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Projects(models.Model):
+    author = models.ForeignKey('auth.User')
+    project_title = models.CharField(max_length=200)
+    project_image_url = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.project_title
--- blog/views.py
+++ blog/views.py
@@ -4,17 +4,23 @@ from django.http import Http404,HttpResponse,HttpResponseRedirect
 from django.template import RequestContext, loader
 
 from django.shortcuts import get_object_or_404 , render
-from .models import Post
+from .models import Post , Projects
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def indi_post(request , post_id):
 
+def indi_post(request, post_id):
     try:
         post = Post.objects.get(pk=post_id)
     except Post.DoesNotExist:
         raise Http404("Post does not exist")
 
     return render(request, 'blog/indi_post.html', {'post' : post})
+
+
+def project_list(request):
+    projects = Projects.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/project_list.html', {'projects': projects})
