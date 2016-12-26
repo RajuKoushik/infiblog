cab9d3ee794a17394d770b27b5faba2e4b64dbd5
rajukoushik <g.rajukoushik@gmail.com>
2016-12-26 23:11:12 +0530

    Updated Projects

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<post_id>[0-9]+)/$', views.indi_post , name='indi_post'),
+    url(r'^projects/', views.project_list, name='project_list'),
 ]
