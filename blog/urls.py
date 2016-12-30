cf9654a5f2a8a671331149e4fcd9b90b2500be4b
rajukoushik <g.rajukoushik@gmail.com>
2016-12-30 17:42:34 +0530

    Final Commit excluding Projects integration

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.home, name='home'),
     url(r'^(?P<post_id>[0-9]+)/$', views.indi_post , name='indi_post'),
     url(r'^projects/', views.project_list, name='project_list'),
     url(r'^home/', views.home, name='home'),
