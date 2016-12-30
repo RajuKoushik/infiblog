40cb0346d85d5a99e298078623c7eb2bb5f8d05f
rajukoushik <g.rajukoushik@gmail.com>
2016-12-30 18:08:28 +0530

    Minor Glitches

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^(?P<post_id>[0-9]+)/$', views.indi_post , name='indi_post'),
     url(r'^projects/', views.project_list, name='project_list'),
-    url(r'^home/', views.home, name='home'),
+    url(r'^home/', views.home, name='home1'),
     url(r'^portfolio/', views.portfolio, name='portfolio'),
 ]
