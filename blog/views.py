eb82c6e9467b475138795ebc1e242b949b257db7
rajukoushik <g.rajukoushik@gmail.com>
2017-04-12 13:24:51 +0530

    Minor glitches.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.utils import timezone
-from django.http import Http404,HttpResponse,HttpResponseRedirect
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 
-from django.shortcuts import get_object_or_404 , render
-from .models import Post , Projects
+from django.shortcuts import get_object_or_404, render
+from .models import Post, Projects
 
 
 def post_list(request):
@@ -18,7 +18,7 @@ def indi_post(request, post_id):
     except Post.DoesNotExist:
         raise Http404("Post does not exist")
 
-    return render(request, 'blog/indi_post.html', {'post' : post})
+    return render(request, 'blog/indi_post.html', {'post': post})
 
 
 def project_list(request):
@@ -33,6 +33,3 @@ def home(request):
 
 def portfolio(request):
     return render(request, 'blog/portfolio.html')
-
-
-
